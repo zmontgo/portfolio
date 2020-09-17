dc3b80f7cba9a2b3b9ac1218a9ad4e04afd3ec77
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-09-17 12:06:33 -0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {
     console.log(window.pageYOffset);
     var nav = document.getElementById('site-header');
-    if ( window.pageYOffset > 100 ) {
+    if ( window.pageYOffset > 10 ) {
         nav.classList.add("header-solid");
     } else {
         nav.classList.remove("header-solid");
