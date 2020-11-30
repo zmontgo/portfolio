e83ad6b61d652881894460f5cbe56664eb0ff359
zmontgo <zmontgo@users.noreply.github.com>
2020-11-30 18:43:03 +0000

    jekyll build from Action a32c372cdb2c6c5bb76b91512f713f4a8b8cea77

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+/*window.onscroll = function() {
+    console.log(window.pageYOffset);
+    var nav = document.getElementById('site-header');
+    if ( window.pageYOffset > 10 ) {
+        nav.classList.add("header-solid");
+    } else {
+        nav.classList.remove("header-solid");
+    }
+}*/
